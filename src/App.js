97e3f638c6890367f1d91d202badbd7c71666be5
Luke Firth <lukeantonfirth@gmail.com>
2019-07-28 09:18:30 +1000

    navbar created

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import React, { Fragment } from "react";
 import { Switch, Route } from "react-router-dom";
+
 import HomePage from "./pages/homepage/HomePage";
 import ShopPage from "./pages/shop/ShopPage";
+import NavBar from "./components/layout/NavBar";
+
 import "./App.scss";
 
 const App = () => {
   return (
     <Fragment>
       <div>
+        <NavBar />
         <Switch>
           <Route exact path="/" component={HomePage} />
           <Route exact path="/shop" component={ShopPage} />
