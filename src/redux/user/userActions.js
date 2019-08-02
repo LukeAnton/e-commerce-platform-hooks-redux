a0cad1b1c3cd6269b9aca79353869ea40118de51
Luke Firth <lukeantonfirth@gmail.com>
2019-08-02 10:50:00 +1000

    userAction destructure

--- src/redux/user/userActions.js
+++ src/redux/user/userActions.js
@@ -1,6 +1,7 @@
 import { UserActionTypes } from "./userTypes";
+const { SET_CURRENT_USER } = UserActionTypes;
 
 export const setCurrentUser = user => ({
-  type: UserActionTypes.SET_CURRENT_USER,
+  type: SET_CURRENT_USER,
   payload: user
 });
