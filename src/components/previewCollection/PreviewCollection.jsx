8d6e317b00cdb696cbb552bc23086fedf43b4f13
Luke Firth <lukeantonfirth@gmail.com>
2019-08-03 16:00:46 +1000

    Item Rewire

--- src/components/previewCollection/PreviewCollection.jsx
+++ src/components/previewCollection/PreviewCollection.jsx
@@ -1,19 +1,18 @@
 import React from "react";
 import CollectionItem from "../collectionItem/CollectionItem";
 import "./previewCollection.scss";
-const PreviewCollection = ({ title, items }) => {
-  return (
-    <div className="collection-preview">
-      <h1 className="title">{title.toUpperCase()}</h1>
-      <div className="preview">
-        {items
-          .filter((item, index) => index < 4)
-          .map(({ id, ...otherItemProps }) => (
-            <CollectionItem key={id} {...otherItemProps} />
-          ))}
-      </div>
+
+const PreviewCollection = ({ title, items }) => (
+  <div className="collection-preview">
+    <h1 className="title">{title.toUpperCase()}</h1>
+    <div className="preview">
+      {items
+        .filter((item, index) => index < 4)
+        .map(item => (
+          <CollectionItem key={item.id} item={item} />
+        ))}
     </div>
-  );
-};
+  </div>
+);
 
 export default PreviewCollection;
--- src/redux/cart/cartReducer.js
+++ src/redux/cart/cartReducer.js
@@ -3,7 +3,7 @@ const { TOGGLE_CART_HIDDEN, ADD_ITEM } = CartActionTypes;
 
 const INITIAL_STATE = {
   hidden: true,
-  cartitems: []
+  cartItems: []
 };
 
 const cartReducer = (state = INITIAL_STATE, action) => {
@@ -13,12 +13,11 @@ const cartReducer = (state = INITIAL_STATE, action) => {
         ...state,
         hidden: !state.hidden
       };
-    case ADD_ITEM: {
+    case ADD_ITEM:
       return {
         ...state,
         cartItems: [...state.cartItems, action.payload]
       };
-    }
     default:
       return state;
   }
