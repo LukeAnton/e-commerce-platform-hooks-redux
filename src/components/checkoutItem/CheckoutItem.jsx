7153c642f811efcf3bb63c44af06b3d4c9048754
Luke Firth <lukeantonfirth@gmail.com>
2019-08-14 09:25:53 +1000

    redux2

---
+++ src/components/checkoutItem/CheckoutItem.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+import { connect } from "react-redux";
+
+import {
+  clearItemFromCart,
+  addItem,
+  removeItem
+} from "../../redux/cart/cartActions";
+
+import "./checkoutItem.scss";
+
+const CheckoutItem = ({ cartItem, clearItemFromCart, addItem, removeItem }) => {
+  const { name, imageUrl, price, quantity } = cartItem;
+  return (
+    <div className="checkout-item">
+      <div className="image-container">
+        <img src={imageUrl} alt="item" />
+      </div>
+      <span className="name">{name}</span>
+      <span className="quantity">
+        <div onClick={() => removeItem(cartItem)} className="arrow">
+          &#10094;
+        </div>
+        <span className="value">{quantity}</span>
+        <div onClick={() => addItem(cartItem)} className="arrow">
+          &#10095;
+        </div>
+      </span>
+      <span className="price">${price}</span>
+      <div
+        onClick={() => clearItemFromCart(cartItem)}
+        className="remove-button"
+      >
+        &#10005;
+      </div>
+    </div>
+  );
+};
+
+export default connect(
+  null,
+  { clearItemFromCart, addItem, removeItem }
+)(CheckoutItem);
--- src/pages/checkout/CheckoutPage.jsx
+++ src/pages/checkout/CheckoutPage.jsx
@@ -1,11 +1,17 @@
 import React from "react";
 import { connect } from "react-redux";
 import { createStructuredSelector } from "reselect";
-import { selectCartItems } from "../../redux/cart/cartSelector";
+
+import CheckoutItem from "../../components/checkoutItem/CheckoutItem";
+
+import {
+  selectCartItems,
+  selectCartTotal
+} from "../../redux/cart/cartSelector";
 
 import "./checkoutPage.scss";
 
-const CheckoutPage = () => {
+const CheckoutPage = ({ cartItems, total }) => {
   return (
     <div className="checkout-page">
       <div className="checkout-header">
@@ -25,12 +31,19 @@ const CheckoutPage = () => {
           <span>Remove</span>
         </div>
       </div>
+      {cartItems.map(cartItem => (
+        <CheckoutItem key={cartItem.id} cartItem={cartItem} />
+      ))}
+      <div className="total">
+        <span>TOTAL: ${total}</span>
+      </div>
     </div>
   );
 };
 
 const mapStateToProps = createStructuredSelector({
-  cartItems: selectCartItems
+  cartItems: selectCartItems,
+  total: selectCartTotal
 });
 
 export default connect(mapStateToProps)(CheckoutPage);
