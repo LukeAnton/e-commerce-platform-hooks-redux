6f521c4801b4b95d1645dedf36faf2e46214fd4a
Luke Firth <lukeantonfirth@gmail.com>
2019-08-15 15:20:35 +1000

    env set

--- src/redux/store.js
+++ src/redux/store.js
@@ -4,7 +4,11 @@ import logger from "redux-logger";
 
 import rootReducer from "./rootReducer";
 
-const middlewares = [logger];
+const middlewares = [];
+
+process.env.NODE_ENV === "development"
+  ? middlewares.push(logger)
+  : middlewares.push(null);
 
 export const store = createStore(rootReducer, applyMiddleware(...middlewares));
 
