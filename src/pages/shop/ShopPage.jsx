3e67e6e5904ad11e6ebaf1a83cb1c3bd0aebaf54
Luke Firth <lukeantonfirth@gmail.com>
2019-08-18 13:48:29 +1000

    shopData Actions added

--- src/pages/shop/ShopPage.jsx
+++ src/pages/shop/ShopPage.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Route } from "react-router-dom";
+import { connect } from "react-redux";
 
 import CollectionsOverview from "../../components/collectionsOverview/CollectionsOverview";
 import CollectionPage from "../collection/CollectionPage";
@@ -9,15 +10,18 @@ import {
   convertCollectionsSnapshotToMap
 } from "../../firebase/firebase";
 
+import { updateCollections } from "../../redux/shop/shopActions";
+
 class ShopPage extends React.Component {
   unsubscribeFromSnapshot = null;
 
   componentDidMount() {
-    const collectionRef = firestore.collection("collection");
+    const { updateCollections } = this.props;
+    const collectionRef = firestore.collection("collections");
 
     this.unsubscribeFromSnapshot = collectionRef.onSnapshot(async snapshot => {
       const collectionsMap = convertCollectionsSnapshotToMap(snapshot);
-      console.log(collectionsMap);
+      updateCollections(collectionsMap);
     });
   }
   render() {
@@ -34,4 +38,12 @@ class ShopPage extends React.Component {
   }
 }
 
-export default ShopPage;
+const mapDispatchToProps = dispatch => ({
+  updateCollections: collectionsMap =>
+    dispatch(updateCollections(collectionsMap))
+});
+
+export default connect(
+  null,
+  mapDispatchToProps
+)(ShopPage);
--- src/redux/shop/shopReducer.js
+++ src/redux/shop/shopReducer.js
@@ -1,11 +1,20 @@
 import SHOP_DATA from "./shopData";
 
+import ShopActionTypes from "./shopTypes";
+const { UPDATE_COLLECTIONS } = ShopActionTypes;
+
 const INITIAL_STATE = {
   collections: SHOP_DATA
 };
 
 const shopReducer = (state = INITIAL_STATE, action) => {
   switch (action.type) {
+    case UPDATE_COLLECTIONS:
+      return {
+        ...state,
+        collections: action.payload
+      };
+
     default:
       return state;
   }
