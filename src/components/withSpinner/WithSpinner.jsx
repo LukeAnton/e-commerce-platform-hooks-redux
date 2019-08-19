dc88fa967518cdb720ec6578a20fc027f149a1b5
Luke Firth <lukeantonfirth@gmail.com>
2019-08-19 15:15:23 +1000

    HOC Spinner

--- src/components/withSpinner/WithSpinner.jsx
+++ src/components/withSpinner/WithSpinner.jsx
@@ -1,14 +1,17 @@
 import React from "react";
 import { SpinnerContainer, SpinnerOverlay } from "./withSpinnerStyles";
 
-const WithSpinner = WrappedComponent => ({isLoading, ...otherProps}) => {
+const WithSpinner = WrappedComponent => {
+  const Spinner = ({ isLoading, ...otherProps }) => {
     return isLoading ? (
-        <SpinnerOverlay>
-        <SpinnerContainer/>
-        </SpinnerOverlay>
-        ) : (
-        <WrappedComponent {...otherProps}
-        );
-    }       
+      <SpinnerOverlay>
+        <SpinnerContainer />
+      </SpinnerOverlay>
+    ) : (
+      <WrappedComponent {...otherProps} />
+    );
+  };
+  return Spinner;
+};
 
 export default WithSpinner;
--- src/pages/shop/ShopPage.jsx
+++ src/pages/shop/ShopPage.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import { Route } from "react-router-dom";
 import { connect } from "react-redux";
 
+import WithSpinner from "../../components/withSpinner/WithSpinner";
+
 import CollectionsOverview from "../../components/collectionsOverview/CollectionsOverview";
 import CollectionPage from "../collection/CollectionPage";
 
@@ -12,7 +14,14 @@ import {
 
 import { updateCollections } from "../../redux/shop/shopActions";
 
+const CollectionsOverviewWithSpinner = WithSpinner(CollectionsOverview);
+const CollectionPageWithSpinner = WithSpinner(CollectionPage);
+
 class ShopPage extends React.Component {
+  state = {
+    loading: true
+  };
+
   unsubscribeFromSnapshot = null;
 
   componentDidMount() {
@@ -22,16 +31,26 @@ class ShopPage extends React.Component {
     this.unsubscribeFromSnapshot = collectionRef.onSnapshot(async snapshot => {
       const collectionsMap = convertCollectionsSnapshotToMap(snapshot);
       updateCollections(collectionsMap);
+      this.setState({ loading: false });
     });
   }
   render() {
     const { match } = this.props;
+    const { loading } = this.state;
     return (
       <div className="shop-page">
-        <Route exact path={`${match.path}`} component={CollectionsOverview} />
+        <Route
+          exact
+          path={`${match.path}`}
+          render={props => (
+            <CollectionsOverviewWithSpinner isLoading={loading} {...props} />
+          )}
+        />
         <Route
           path={`${match.path}/:collectionId`}
-          component={CollectionPage}
+          render={props => (
+            <CollectionPageWithSpinner isLoading={loading} {...props} />
+          )}
         />
       </div>
     );
